f14f0033b0962fa6f3c8df3b719725113408039f
Holden Vail <112910214+HoldenV@users.noreply.github.com>
2023-11-16 22:32:29 -0600

    helper functions completed

--- Implementation/main.cpp
+++ Implementation/main.cpp
@@ -7,80 +7,157 @@ Last Updated: 14 November 2023
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stdexcept>                                    //Make sure to use for error catching
+#include <stdexcept>                                    //Use  stdexcept for error catching
 
 using namespace std;
 
 string collectInput();                                  // Declaring the functions in the program
-string preValidateInput(string userInput);
-void vectorify(string inputString);
+void preValidateInput(string userInput);
+vector<vector<char> > vectorify(string inputString);
+bool isValid(vector<vector<char> > input);              //Input is in the first index, key is in the second
+void printOutput(vector<char> output);
+
 
 int main(){
     /*This is the main function for the program*/
-    string userInput = collectInput();                  // Collecting the user input
-    string validInput = preValidateInput(userInput);    // Validating the user input
-    vectorify(validInput);                              // Simplifying the user input
-
-    cout << validInput << endl;                         // Just a test print for the time being. Will be removed later
-
+    string userInput;
+    while (true){
+        userInput = collectInput();             //Collects user input as a string userInput
+        if (userInput == "exit"){
+            break;
+        }
+        try {
+            preValidateInput(userInput);           //Performs a pre-check for invalid characters
+        } catch (invalid_argument& e){
+            cout << "Invalid input" << endl;
+            continue;
+        }
+        vector<vector<char> > vectorifiedOutput = vectorify(userInput); //Vectorifies the input to a vector of vectors vectorifiedOutput
+        if (isValid(vectorifiedOutput) == true){                        //If valid, your program below...
+            cout << "Valid" << endl;
+        }else{
+            cout << "Invalid" << endl;
+        }
+    }
     return 0;
 }
 
 string collectInput(){
     /*This is the function that collects the input from the user*/
     string input;
-    cout << "Enter an expression: ";
+    cout << "Enter an expression (or 'exit'): ";
     getline(cin, input);
 
     return input;
 }
 
-string preValidateInput(string userInput){
+void preValidateInput(string userInput){
     /*This is the function that does a pre-check for invalid characters*/
-    string validCharList = "0123456789+-*/()^";
+    string validCharList = "0123456789+-*/()^%";
     string validInput;
 
-    for (char i : userInput){
-        if (validCharList.find(i) != string::npos){
-            validInput.push_back(i);
+    for (int i = 0; i < userInput.size(); i++){
+        if (validCharList.find(userInput[i]) != string::npos){
+            validInput.push_back(userInput[i]);
         }
     }
-    return validInput;
-}
-
-void output(vector<char> output){
-    /*This is the function that outputs the final result*/
-    for (char i : output){
-        cout << i;
+    if (validInput != userInput) {
+        throw invalid_argument("Invalid input");
     }
 }
 
-void vectorify(string inputString){
-    /*This function simplifies the strings for furhter use*/
-    vector<char> output;
+vector<vector<char> > vectorify(string inputString){
+    /*This function puts the input into a vector and creates a value independent 'key' copy of the user vector*/
+    vector<vector<char> > output;
+    vector<char> input;
     vector<char> key;
 
-    for (char i : inputString){
-        output.push_back(i);
+    for (int i = 0; i < inputString.size(); i++){
+        input.push_back(inputString[i]);
     }
 
-    for (char i : inputString){
-        if (i == '1' || i == '2' || i == '3' || i == '4' || i == '5' || i == '6' || i == '7' || i == '8' || i == '9'){
+    for (int i = 0; i < inputString.size(); i++){
+        if (inputString[i] == '1' || inputString[i] == '2' || inputString[i] == '3' || inputString[i] == '4' || inputString[i] == '5' || inputString[i] == '6' || inputString[i] == '7' || inputString[i] == '8' || inputString[i] == '9'){
             key.push_back('0');
+        }else{
+            key.push_back(inputString[i]);
         }
     }
-    for (char i : inputString){
-        key.push_back(i);
-    }
+    output.push_back(input);
+    output.push_back(key);
+    return output;
 }
 
-/* 
-Notes:
+bool isValid(vector<vector<char> > inputVecVec){
+    /*
+    Valid statements must adhere to the following the the following only:
+    1. For each opening paranthesis, there must be a closing paranthesis and vice versa
+    2. The following operators must have an interger (or parenthesis) on either side: *, /, %, ^
+    3. The following operators must have an interger (or parenthesis) on at least their right side: +, -
+    4. An opening parenthisis must not have an interer on its left side
+    5. A closing paranthesis must not have an integer on its right side
+    */  
+    vector<char> key = inputVecVec[1];
+    for (int i = key.size() - 1; i > 0; i--){           //This loop reduces all adjacent 0's to one 0 in key
+        if (key[i] == key[i-1] && key[i] == '0'){
+            key.erase(key.begin() + i);
+            i--;
+        }
 
-the input preVaildation program does not error when the user inputs a string with invalid characters 
+    }
+    int openCount = 0;
+    int closeCount = 0;
+    for (int i = 0; i < key.size(); i++){       //This loop checks for equal number of open and close paranthesis
+        if (key[i] == ')'){
+            closeCount++;
+        }
+        if (key[i] == '('){
+            openCount++;
+        }
+    if (openCount != closeCount){
+        return false;
+    }
+    }
+    for (int i = 0; i < key.size(); i++){
+        if (key[i] == '*' || key[i] == '/' || key[i] == '%' || key[i] == '^'){
+            try {
+                if (key[i-1] != '0' || key[i+1] != '0'){
+                    return false;
+                }
+            }catch (out_of_range& e){
+                return false;
+            }
+        }
+    }
+    for (int i = 0; i < key.size(); i++){
+        if (key[i] == '+' || key[i] == '-'){
+            try {
+                if (key[i+1] != '0'){
+                    return false;
+                }
+            }catch (out_of_range& e){
+                return false;
+            }
+        }
+    }
+    for (int i = 0; i < key.size(); i++){
+        if (key[i] == '(' && i != 0){
+            if (key[i-1] != '0'){
+                return false;
+            }
+        }
+        if (key[i] == ')' && i != key.size() - 1){
+            if (key[i+1] != '0'){
+                return false;
+            }
+        }
+    } 
+    return true;     
+}
 
-the vecotrify function  returns turns the string into two vectors. The first contains user input
-the second contains only the operators, parentheses, and zeroes in place of numbers.
-
-I have not yet done the full checkVaild function.
-*/
+void printOutput(vector<char> output){
+    /*This is the function that outputs the final result*/
+    for (int i = 0; i < output.size(); i++){
+        cout << output[i];
+    }
+}
