02969192cc579e1a8ebe712fb7e6d8a0b39ee514
JackWSL <jgb1017@gmail.com>
2023-11-19 21:58:31 -0600

    Partial implementation of nextStep

--- Implementation/main.cpp
+++ Implementation/main.cpp
@@ -18,7 +18,7 @@ vector<vector<char> > vectorify(string inputString);
 bool isValid(vector<vector<char> > input);              //Input is in the first index, key is in the second
 void printOutput(vector<char> output);
 void solveStep(vector<int>& numbers, vector<char>& key, int start_index);
-
+void nextStep(vector<char> &keyVec, vector<int> &intVec);
 
 int main(){
     /*This is the main function for the program*/
@@ -196,5 +196,92 @@ void solveStep(vector<int>& numbers, vector<char>& key, int start_index) {
         numbers.erase(numbers.begin() + start_index + 1);
         key.erase(key.begin() + start_index + 2);
         key.erase(key.begin() + start_index + 1);
+    }
+}
+
+// Input a 2D vector that contains the key and the expression
+// Outputs nothing, but calls solveStep
+// Input further defined --> 3+2
+//      intVector - integers, Cannot be used to know where there is an operator, as we may have zeroes --> [302] - int vec
+//      keyVector - 0 in place of integer, otherwise symbol --> [0+0] - char vec
+void nextStep(vector<char> &keyVec, vector<int> &intVec) {
+    // Find the index of the beginning of the next step
+
+    // Case 1- Is there an instance (num) - pass out [(num)] --> e.g. (3)
+    // keyVec.size() - 1 = the last index of keyVecs
+    for (int i = 0; i <=  keyVec.size() - 1; i++) {
+        if (keyVec[i] == '(') {
+            if (keyVec[i+2] == ')') {
+                // pass keyVec[i] - the start parenthesis
+                solveStep(intVec, keyVec, i);
+                return;
+            }
+        }
+    }
+
+    // Case 2 - Is there an instance of (expression) - always take first found, but farthest in
+    //      Iterate through and find last open, then find first close, and return last open index
+    //      If have (3+4), pass out [expression] --> e.g. 3+4
+    //      Case ((, case (3), case (3+4)
+    for (int i = 0; i <= keyVec.size() - 1; i++) {
+        if (keyVec[i] == '(') {
+            if (keyVec[i+1] == '(') continue; // Case (( --> skip iteration, move to the next one in
+            if (keyVec[i+2] == ')') continue; // Case (3) --> should be handled above, but check just in case
+            if (keyVec[i+2] != '0') { // Case ([NUM][OPERATOR][NUM]), ) already checked
+                // pass keyVec[i+1] - the start expression number
+                solveStep(intVec, keyVec, i+1);
+                return;
+            }
+        }
+    }
+
+    // Case 3 - num-operator-num --> e.g. 3*2
+    //      Handle in order of PEMDAS
+    //      Valid operators --> [+, -, *, /, %, ^]   
+    for (int i = 0; i < keyVec.size() - 2; i++) {
+        if (keyVec[i] == '0' && keyVec[i+1] != '0' && keyVec[i+1] != '(' && keyVec[i+1] != ')' && keyVec[i+2] == '0') {
+            if (keyVec[i+1] == '^') {
+                solveStep(intVec, keyVec, i);
+                return;
+            }
+        }
+    }
+
+    for (int i = 0; i < keyVec.size() - 2; i++) {
+        if (keyVec[i] == '0' && keyVec[i+1] != '0' && keyVec[i+1] != '(' && keyVec[i+1] != ')' && keyVec[i+2] == '0') {
+            if (keyVec[i+1] == '*' or keyVec[i+1] == '/' or keyVec[i+1] == '%') {
+                if (keyVec[i+1] == '^') {
+                    solveStep(intVec, keyVec, i);
+                    return;
+                }
+                else if (keyVec[i+1] == '*') {
+                    solveStep(intVec, keyVec, i);
+                    return;
+                }
+                else if (keyVec[i+1] == '/') {
+                    solveStep(intVec, keyVec, i);
+                    return;
+                }
+                else if (keyVec[i+1] == '%') {
+                    solveStep(intVec, keyVec, i);
+                    return;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < keyVec.size() - 2; i++) {
+        if (keyVec[i] == '0' && keyVec[i+1] != '0' && keyVec[i+1] != '(' && keyVec[i+1] != ')' && keyVec[i+2] == '0') {
+            if (keyVec[i+1] == '+' || keyVec[i+1] == ('-')) {
+                if (keyVec[i+1] == '+') {
+                    solveStep(intVec, keyVec, i);
+                    return;
+                }
+                else if (keyVec[i+1] == '-') {
+                    solveStep(intVec, keyVec, i);
+                    return;
+                }
+            }
+        }
     }
 }
