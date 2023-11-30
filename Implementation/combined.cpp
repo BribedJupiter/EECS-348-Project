2f9349e002937b8937a5fb84178b97aa42a4bfa4
JackWSL <jgb1017@gmail.com>
2023-11-30 13:15:13 -0600

    Added unary support to next step

--- Implementation/combined.cpp
+++ Implementation/combined.cpp
@@ -328,16 +328,12 @@ void solveStep(vector<double>& numbers, vector<char>& key, int start_index) {
     }
 }
 
-// Input a 2D vector that contains the key and the expression
-// Outputs nothing, but calls solveStep
-// Input further defined --> 3+2
-//      intVector - integers, Cannot be used to know where there is an operator, as we may have zeroes --> [302] - int vec
-//      keyVector - 0 in place of integer, otherwise symbol --> [0+0] - char vec
+// Input key vector and double vector
+// Outputs the beginning index  of the part of the expression that needs to be simplified next
 int nextStep(vector<char>& keyVec, vector<double>& doubVec) {
     /* Find the index of the beginning of the next step */
 
-    // Case 1- Is there an instance (num) - pass out [(num)] --> e.g. (3)
-    // keyVec.size() - 1 = the last index of keyVecs
+    // Case 1 --> (number)
     for (int i = 0; i <=  keyVec.size() - 1; i++) {
         if (keyVec[i] == '(') {
             if (keyVec[i+2] == ')') {
@@ -347,28 +343,27 @@ int nextStep(vector<char>& keyVec, vector<double>& doubVec) {
         }
     }
 
-    // Case 2 - Is there an instance of (expression) - always take first found, but farthest in
-    //      Iterate through and find last open, then find first close, and return last open index
-    //      If have (3+4), pass out [expression] --> e.g. 3+4
-    //      Case ((, case (3), case (3+4)
+    // Case 2 --> (expression)
     for (int i = 0; i <= keyVec.size() - 1; i++) {
         if (keyVec[i] == '(') {
-            if (keyVec[i+1] == '(') continue; // Case (( --> skip iteration, move to the next one in
-            if (keyVec[i+2] == ')') continue; // Case (3) --> should be handled above, but check just in case
-            if (keyVec[i+2] != '0') { // Case ([NUM][OPERATOR][NUM]), ) already checked
-                // pass keyVec[i+1] - the start expression number
+            if (keyVec[i+1] == '(') continue; // Case ((
+            if (keyVec[i+2] == ')') continue; // Case (number)
+            if (keyVec[i+2] != '0') { // Case (expression)
                 return i+1;
             }
         }
     }
 
-    // Case 3 - num-operator-num --> e.g. 3*2 or 5^1
-    //      Handle in order of operations with modulo having the same precedence as multiplication and division.
-    //      Valid operators --> [+, -, *, /, %, ^]
-    //      Three groups of precedence. Each group is evaluated from left to right
-    //                1: Exponentials: e.g. 5^3
-    //                2: Multiplication / Division / Modulo: e.g. 3*2, 8/4, and 7%3 
-    //                3: Addition / Subtraction: e.g. 6+9 or 2-1
+    // Case 3 --> -number or +number (unary operator)
+    for (int i = 0; i <= keyVec.size() - 1; i++) {
+        if (keyVec[i] == '-' || keyVec[i] == '+') {
+            if (keyVec[i+1] == '0') {
+                return i;
+            }
+        }
+    }
+
+    // Case 4 - PEMDAS
     for (int i = 0; i < keyVec.size() - 2; i++) { // Iterates through the key vector
         if (keyVec[i] == '0' && keyVec[i+1] != '0' && keyVec[i+1] != '(' && keyVec[i+1] != ')' && keyVec[i+2] == '0') { // Checks for an instance of num-operator-num
             if (keyVec[i+1] == '^') { // If there is an instance of an exponential, it determines it to be the next step and returns the index of the first number in the grouping
